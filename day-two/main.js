669a761d2f422e59558e23746116c73697d268a8
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-01-23 00:05:56 -0800

    analog clock

--- day-two/main.js
+++ day-two/main.js
@@ -1,10 +1,20 @@
-const secondHand = document.querySelector('.second-hand')
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
 function setDate() {
     const current = new Date();
     const seconds = current.getSeconds();
-    const secondsDegrees = ((seconds / 90) * 360);
+    const secondsDegrees = ((seconds / 90) * 360) + 90;
     secondHand.style.transform =  `rotate(${secondsDegrees}deg)`;
+
+    const mins = current.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + 90;
+    minuteHand.style.transform = `rotate(${minsDegrees}deg)`
+
+    const hours = current.getHours();
+    const hoursDegrees = ((hours / 12) * 360) + 90;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
 }
 
 setInterval(setDate, 1000)
