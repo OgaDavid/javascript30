25eaeb4ccdc87dd2b3f437850b634681e80737a2
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-02-07 21:52:27 -0800

    edit

--- day-three/main.js
+++ day-three/main.js
@@ -1,3 +1,13 @@
+// const inputs = document.querySelectorAll('.controls input')
+
+// function handleUpdate() {
+//     console.log(this.value);
+// }
+
+//  inputs.forEach(input => {
+//     input.addEventListener('mousemove', handleUpdate);
+//  })
+
 const inputs = document.querySelectorAll('.controls input')
 
 function handleUpdate() {
